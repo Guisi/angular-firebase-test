f5d8fc8b12c1adf86d65763c02354d30cef781f4
Douglas Guisi <guisiagudos@gmail.com>
2019-05-30 21:54:13 -0300

    Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
 
   firebaseConfig: {
-    apiKey: 'AIzaSyDS6bqCBOynufGVkzZqxaOb2gHZ2tKNt2M',
-    authDomain: 'teste-baby-names-db.firebaseapp.com',
-    databaseURL: 'https://teste-baby-names-db.firebaseio.com',
-    projectId: 'teste-baby-names-db',
-    storageBucket: 'teste-baby-names-db.appspot.com',
-    messagingSenderId: '548973778454',
-    appId: '1:548973778454:web:34cd0b821f067311'
+    apiKey: 'AIzaSyBRufAywlixCOhr1D0e-CZfpe7-8TdiCE4',
+    authDomain: 'fir-guisi.firebaseapp.com',
+    databaseURL: 'https://fir-guisi.firebaseio.com',
+    projectId: 'fir-guisi',
+    storageBucket: 'fir-guisi.appspot.com',
+    messagingSenderId: '275312953761',
+    appId: '1:275312953761:web:38e56c74e6031cc7'
   }
 };
